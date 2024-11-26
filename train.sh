a6f6e61f2922233bde50c1a180670fbb7f887091
unknown <sudy7info@gmail.com>
2024-11-27 01:26:54 +0900

    fix

--- train.sh
+++ train.sh
@@ -1,7 +1,7 @@
-export HF_HOME=/workspace
+export HF_HOME=~/
 export CUDA_LAUNCH_BLOCKING=1
 export TORCH_USE_CUDA_DSA=1
 export MASTER_ADDR=10.1.201.02
 export MASTER_PORT=29500
 
-deepspeed --hostfile=hostfile --num_nodes=4 --num_gpus=8 finetune_new.py --deepspeed ds_config_new.json
+deepspeed --hostfile=hostfile --num_nodes=4 --num_gpus=8 finetune_mn.py --deepspeed ds_config_mn.json
