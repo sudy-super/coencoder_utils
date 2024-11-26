a673949fd7f85b0c50524c95dc6fd9e7270eedbd
unknown <sudy7info@gmail.com>
2024-11-26 20:08:06 +0900

    add master_addr in bash file

--- train.sh
+++ train.sh
@@ -1,5 +1,6 @@
 export HF_HOME=/workspace
 export CUDA_LAUNCH_BLOCKING=1
 export TORCH_USE_CUDA_DSA=1
+export MASTER_ADDR=10.1.201.02
 
-deepspeed --hostfile=hostfile --num_nodes=4 --num_gpus=8 finetune_mn.py --deepspeed ds_config_mn.json
+deepspeed --hostfile=hostfile --num_nodes=4 --num_gpus=8 finetune_new.py --deepspeed ds_config_new.json
