161b943741392eaf45604e483218682da755218f
unknown <sudy7info@gmail.com>
2024-11-26 22:03:16 +0900

    change hostname and add master port

--- train.sh
+++ train.sh
@@ -2,5 +2,6 @@ export HF_HOME=/workspace
 export CUDA_LAUNCH_BLOCKING=1
 export TORCH_USE_CUDA_DSA=1
 export MASTER_ADDR=10.1.201.02
+export MASTER_PORT=29500
 
 deepspeed --hostfile=hostfile --num_nodes=4 --num_gpus=8 finetune_new.py --deepspeed ds_config_new.json
