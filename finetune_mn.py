45b60fb7e5ed86fb364d7de8c96801b38a2a484c
unknown <sudy7info@gmail.com>
2024-12-17 23:37:32 +0900

    Fix training arguments

--- finetune_mn.py
+++ finetune_mn.py
@@ -124,7 +124,7 @@ def generate_inputs(batch):
         text = """<|begin_of_text|><|start_header_id|>system<|end_header_id|>
 
 Cutting Knowledge Date: December 2023
-Today Date: 15 Dec 2024
+Today Date: 17 Dec 2024
 
 <|eot_id|>"""
         for c in conversations:
@@ -613,9 +613,9 @@ logging.enable_progress_bar()
 # トレーニング引数の設定
 args = TrainingArguments(
     num_train_epochs=1,
-    per_device_train_batch_size=1 if phase==2 else 2,
-    per_device_eval_batch_size=1 if phase==2 else 2,
-    gradient_accumulation_steps=2 if phase==2 else 2, # Phase1: 2, Phase2: 1
+    per_device_train_batch_size=1 if phase==2 else 1,
+    per_device_eval_batch_size=1 if phase==2 else 1,
+    gradient_accumulation_steps=2 if phase==2 else 4, # Phase1: 2, Phase2: 1
     learning_rate=2e-5 if phase==2 else 1e-3, # Phase1: 1e-3, Phase2: 2e-5
     # label_smoothing_factor=0.1 if phase==2 else 0.0,
     adam_beta2=0.95,
