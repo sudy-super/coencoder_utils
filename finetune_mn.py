735ac47e307a341602cfb6860728378494debda4
unknown <sudy7info@gmail.com>
2024-12-18 08:24:02 +0900

    Fix training arguments

--- finetune_mn.py
+++ finetune_mn.py
@@ -613,8 +613,8 @@ logging.enable_progress_bar()
 # トレーニング引数の設定
 args = TrainingArguments(
     num_train_epochs=1,
-    per_device_train_batch_size=1 if phase==2 else 1,
-    per_device_eval_batch_size=1 if phase==2 else 1,
+    per_device_train_batch_size=1,
+    per_device_eval_batch_size=1,
     gradient_accumulation_steps=2 if phase==2 else 4, # Phase1: 2, Phase2: 1
     learning_rate=2e-5 if phase==2 else 1e-3, # Phase1: 1e-3, Phase2: 2e-5
     # label_smoothing_factor=0.1 if phase==2 else 0.0,
