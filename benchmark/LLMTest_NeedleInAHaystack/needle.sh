13a0e20a512da06d06c7414a1ba8cb078dbcbf93
unknown <sudy7info@gmail.com>
2025-01-29 10:29:42 +0900

    Update needle in a haystack

--- benchmark/LLMTest_NeedleInAHaystack/needle.sh
+++ benchmark/LLMTest_NeedleInAHaystack/needle.sh
@@ -3,8 +3,8 @@ needlehaystack.run_test \
     --evaluator openai \
     --model_name "Qwen/Qwen2.5-7B-Instruct" \
     --evaluator_model_name "chatgpt-4o-latest" \
-    --context_lengths_min 1000 \
-    --context_lengths_max 131000 \
-    --context_lengths_num_intervals 27 \
-    --document_depth_percent_intervals 27 \
+    --context_lengths_min 1024 \
+    --context_lengths_max 131072 \
+    --context_lengths_num_intervals 33 \
+    --document_depth_percent_intervals 33 \
     --seconds_to_sleep_between_completions 0.5 \
